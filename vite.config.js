f82b145b2ea25c6fb16d462877022eb019bf2a69
wxm <w05172075@outlook.com>
2025-06-24 15:42:26 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
 	//静态资源路径
 	assetsDir: "assets",
 	//使用相对路径
-	// base: "/GSAP-Playground/",
-	base: "https://wxmvv.github.io/GSAP-Playground/",
+	base: "/GSAP-Playground/",
+	// base: "https://wxmvv.github.io/GSAP-Playground/",
 	// base: "./",
 	//插件
 	plugins: [react(), tailwindcss()],
